62850ec41ff4fd9703767ff972bbd13e78906b38
brandonkhoza <8brandonkhoza@gmail.com>
2023-11-15 03:05:36 +0200

    Custom cursors with react and framer motion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 import './App.css';
 
 function App() {
+
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0
+  });
+
+  const [cursorVariant, setCursorVariant] = useState('default');
+
+
+  useEffect(() => {
+    const mouseMove = e => {
+      setMousePosition({
+        x: e.clientX - 17.5,
+        y: e.clientY - 17.5
+      });
+    }
+
+    document.addEventListener("mousemove", mouseMove);
+    return () => {
+      window.removeEventListener("mousemove", mouseMove)
+    }
+  }, []);
+
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 2,
+      y: mousePosition.y - 2,
+      backgroundColor: "green",
+    },
+    text: {
+      height: 150,
+      width: 150,
+
+      x: mousePosition.x - 75,
+      y: mousePosition.y - 75,
+      backgroundColor: "brown",
+      mixBlendMode: "difference"
+    },
+  }
+
+  const enterCur = () => {
+    setCursorVariant("text");
+  }
+
+
+  const leaveCur = () => {
+    setCursorVariant("default");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 onMouseEnter={enterCur}
+        onMouseLeave={leaveCur}
+        className='title'
+      >Hello World</h1>
+
+      <motion.div
+        className='cursor'
+        variants={variants}
+        animate={cursorVariant}
+      ></motion.div>
     </div>
   );
 }
